31616b375d9822cf2eb149c5908a93dcc48d4ae8
ItIsEternity <lavrovslava207@gmail.com>
2021-11-17 16:40:48 +0300

    Font and img files added

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import arrow from '../img/arrow_icon.svg'
+import wordCounter from '../img/word-counter.gif'
 
 export default function WorkCard() {
     return (
@@ -9,14 +10,15 @@ export default function WorkCard() {
                 <p className='works__card-date'>00 / 00</p>
             </div>
             <div className='works__card__gif-cover'>
-                <div className='works__card__gif'></div>
-                <div className='works__card__gif-cover__text'>
-                    Aenean convallis ultricies mollis. In hac habitasse platea dictumst. Cras metus enim,
-                    mattis quis lacus id, dapibus pulvinar orci. Interdum et malesuada fames ac ante ipsum
-                    primis in faucibus. Proin laoreet sit amet lorem a vestibulum. Nulla quis consectetur ligula.
-                    Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-                    Maecenas vitae malesuada justo, a eleifend purus.
-                </div>
+                <img src={wordCounter}
+                {/*<div className='works__card__gif'></div>*/}
+                {/*<div className='works__card__gif-cover__text'>*/}
+                {/*    Aenean convallis ultricies mollis. In hac habitasse platea dictumst. Cras metus enim,*/}
+                {/*    mattis quis lacus id, dapibus pulvinar orci. Interdum et malesuada fames ac ante ipsum*/}
+                {/*    primis in faucibus. Proin laoreet sit amet lorem a vestibulum. Nulla quis consectetur ligula.*/}
+                {/*    Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.*/}
+                {/*    Maecenas vitae malesuada justo, a eleifend purus.*/}
+                {/*</div>*/}
             </div>
             <div className='works__card__footer'>
                 <p>
