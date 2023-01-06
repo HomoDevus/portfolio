5f3f4f02b52a652e4ee2e1f5859a1c093566d7e9
ItisEternity <lavrovslava207@gmail.com>
2023-01-06 11:12:50 +0300

    Localization for name and language buttons added

--- src/components/blocks/Main/LangToggleButton.jsx
+++ src/components/blocks/Main/LangToggleButton.jsx
@@ -1,26 +1,26 @@
 import React from 'react'
 import { LOCALES } from '../../../assets/languages/locales'
+import { FormattedMessage } from 'react-intl';
 
 function LangToggleButton({ lang, setLang, buttonLang }) {
-    function langChange() {
-        if (buttonLang !== lang) {
-            setLang(buttonLang)
-            localStorage.setItem('locale', buttonLang)
-        }
+  function langChange() {
+    if (buttonLang !== lang) {
+      setLang(buttonLang)
+      localStorage.setItem('locale', buttonLang)
     }
+  }
 
-    let isButtonChosen = buttonLang === lang
+  let isButtonChosen = buttonLang === lang
 
-    return (
-        <button
-            className={`language-toggle-button ${isButtonChosen && 'chosen'} link`}
-            id={lang === buttonLang ? 'active-lang' : ''}
-            onClick={langChange}
-        >
-            {/*<FormattedMessage id={`menu_lang_${buttonLang === LOCALES.RUSSIAN ? 'ru': 'en'}`} />*/}
-            {buttonLang === LOCALES.RUSSIAN ? 'RUS' : 'ENG'}
-        </button>
-    )
+  return (
+    <button
+      className={`language-toggle-button ${isButtonChosen && 'chosen'} link`}
+      id={lang === buttonLang ? 'active-lang' : ''}
+      onClick={langChange}
+    >
+      {buttonLang === LOCALES.RUSSIAN ? <FormattedMessage id="menu_lang_ru" /> : <FormattedMessage id="menu_lang_en" />}
+    </button>
+  )
 }
 
 export default LangToggleButton
--- src/components/blocks/Main/Main.jsx
+++ src/components/blocks/Main/Main.jsx
@@ -5,42 +5,40 @@ import { useIntl } from 'react-intl'
 import { landingTextLastLine, landingTextBeforeLastLine, landingText } from '../../../constants'
 
 export default function Main() {
-    const Intl = useIntl()
+  const Intl = useIntl()
 
-    document.title = Intl.formatMessage({ id: 'name' })
+  document.title = Intl.formatMessage({ id: 'name' })
 
-    return (
-        <main className="landing-page">
-            <SideMenus>
-                <div className="landing__main-text-container">
-                    <div className="landing__main-text">
-                        {landingText}
-                        <p>
-                            Nam sceler
-                            {/*<span className='landing__main-text__heading'*/}
-                            {/*      data-text='Suspe&nbsp;Praese'>Suspe&nbsp;Praese</span>*/}
-                            <span className="landing__main-text__heading" data-text="Slava&nbsp;Lavrov">
-                                Slava&nbsp;Lavrov
-                            </span>
-                            pharetra consequat. Aliquam dapibus elementum risus vitae euismod. hendrerit id
-                            eget metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. in lorem et
-                            arcu facilisis eget
-                        </p>
-                        {landingTextBeforeLastLine}
-                        <p>
-                            Praes semr
-                            <span
-                                className="landing__main-text__heading"
-                                data-text={Intl.formatMessage({ id: 'profession' })}
-                            >
+  return (
+    <main className="landing-page">
+      <SideMenus>
+        <div className="landing__main-text-container">
+          <div className="landing__main-text">
+            {landingText}
+            <p>
+              Nam sceler
+              <FormattedMessage id="name">
+                {name => <span className="landing__main-text__heading" data-text={name}>{name}</span>}
+              </FormattedMessage>
+              pharetra consequat. Aliquam dapibus elementum risus vitae euismod. hendrerit id
+              eget metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. in lorem et
+              arcu facilisis eget
+            </p>
+            {landingTextBeforeLastLine}
+            <p>
+              Praes semr
+              <span
+                className="landing__main-text__heading"
+                data-text={Intl.formatMessage({ id: 'profession' })}
+              >
                                 <FormattedMessage id="profession" />
                             </span>
-                            el in felis. Curabitur bibendum non sem non pretium.
-                        </p>
-                        {landingTextLastLine}
-                    </div>
-                </div>
-            </SideMenus>
-        </main>
-    )
+              el in felis. Curabitur bibendum non sem non pretium.
+            </p>
+            {landingTextLastLine}
+          </div>
+        </div>
+      </SideMenus>
+    </main>
+  )
 }
