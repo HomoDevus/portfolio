b383bc3d43e5ceb43f4e88f591c3e1b1aabaccd1
ItisEternity <lavrovslava207@gmail.com>
2023-01-06 13:00:10 +0300

    New workplaces added

--- src/assets/languages/messages.js
+++ src/assets/languages/messages.js
@@ -1,104 +1,123 @@
 import { LOCALES } from './locales'
 
 export const MESSAGES = {
-    [LOCALES.ENGLISH]: {
-        name: 'Slava Lavrov',
-        profession: 'Front-end Developer',
-        menu_works: 'WORKS',
-        menu_about: 'ABOUT',
-        menu_lang_ru: 'RUS',
-        menu_lang_en: 'ENG',
-        menu_mail: 'MAIL',
-        project_minesweeper: 'Minesweeper',
-        project_minesweeper_description:
-            "It's a classical game written in JavaScript and React. The objective" +
-            ' of the game is to clear a rectangular board containing hidden mines without detonating any of them, with' +
-            ' help from clues about the number of neighboring mines in each field.',
-        project_piano: 'Virtual Piano',
-        project_piano_description:
-            'This is a virtual piano. Its feature is in the ability to automatically ' +
-            "play thousand of melodies using 'pseudo notes' from the website that contains 'pseudo notes' for " +
-            'thousand of songs.',
-        project_word_counter: 'Word Counter',
-        project_word_counter_description:
-            'An app for word count that can help you learn English by getting the most' +
-            ' used words in a book before reading. It works with .epub files. Its main feature is called "Smart Count"' +
-            ' its ability to count words by their root (words with different forms count as the same one).',
-        technologies: 'technologies I use',
-        yandex_title: 'Yandex',
-        yandex_position: 'Tester Accessor',
-        yandex_description:
-            'Tested browser, mobile applications, web sites API’s using web debugging proxy tools ' +
-            'to mock and modify applications requests.',
-        yandex_href: 'https://yandex.ru',
-        lexicom_title: 'Lexicom',
-        lexicom_position: 'Junior-Middle React Developer',
-        lexicom_description:
-            'Developed on the web text documents system that has templates, text blocks and autosave features.\n' +
-            '\n' +
-            'Made text editor 90% more efficient in text insertion, editing and styling by separating CPU-heavy tasks into macrotasks.\n' +
-            '\n' +
-            'Build state management system with middleware that connected application to the backend API, using Redux Saga.',
-        lexicom_href: 'https://lexicom.ai',
-        about_me_title: 'About me',
-        about_me_text:
-            "I'm an inspired front-end developer. Sometimes I do web-design for my projects, like this website." +
-            ' Currently, I code using TypeScript and React. I have' +
-            " commercial experience in development and I also completed {school_name}. Right now I'm an intern" +
-            ' at Yandex.',
-        yandex_school_name: 'Yandex developer school',
-        contacts: 'Contacts',
-        telegram: 'Telegram',
-        resume: 'Resume',
-        resume_href: '#',
-    },
-    [LOCALES.RUSSIAN]: {
-        name: 'Слава Лавров',
-        profession: 'Front-end Разработчик',
-        menu_works: 'РАБОТЫ',
-        menu_about: 'О СЕБЕ',
-        menu_lang_ru: 'РУС',
-        menu_lang_en: 'АНГ',
-        menu_mail: 'ПОЧТА',
-        project_minesweeper: 'Минёр',
-        project_minesweeper_description:
-            'Это классическая игра написанная на React и JavaScript. Цель игры, открыть все ' +
-            'клетки не содержащие мины. При открытии клетки отображается количество мин в соседних клетках.',
-        project_piano: 'Виртуально пианино',
-        project_piano_description:
-            'Это не просто пианино, его главная особенность в том, что оно может автоматически' +
-            ' проигрывать тысячи мелодий с помощью “псевдо нот”.',
-        project_word_counter: 'Word Counter',
-        project_word_counter_description:
-            'Приложение для подсчёта слов и изучения английского языка с помощью ' +
-            'чтения книг. Работает с файлами расширения .epub. У приложения есть режим "Smart Count" который считает' +
-            ' слова по их корню (слова с разными окончаниями считаются как одно слово).',
-        technologies: 'технологии которые я использую',
-        yandex_title: 'Яндекс',
-        yandex_position: 'Тестировщик Асессор',
-        yandex_description:
-            'Занимался тестированием браузеров, мобильных приложений, веб-сайтов и API. Использовал' +
-            ' инструменты отладки веб прокси для проверки и подменны запросов приложения.',
-        yandex_href: 'https://yandex.ru',
-        lexicom_title: 'Лексиком',
-        lexicom_position: 'Junior-Middle React Разработчик',
-        lexicom_description:
-            'Разработал систему текстовых документов, которая имеет шаблоны, текстовые блоки и авто сохранение.\n' +
-            '\n' +
-            'Оптимизировал текстовый редактор при вставке, редактировании и стилизации большого объёма текста с помощью разделения тяжёлых задач на макрозадачи.\n' +
-            '\n' +
-            'Разработал систему управления состояниям с middleware для отправки запросов на backend используя Redux Saga',
-        lexicom_href: 'https://lexicom.ai',
-        about_me_title: 'О себе',
-        about_me_text:
-            'Я frontend разработчик, иногда делаю дизайн для своих проектов, как например этот сайт. ' +
-            'Пишу код на TypeScript и React. У меня есть коммерческий опыт в разработки и pet проекты. ' +
-            'Я окончил {school_name} и теперь стажируюсь здесь.' +
-            '',
-        yandex_school_name: 'школу разработки интерфейсов Яндекса',
-        contacts: 'Контакты',
-        telegram: 'Телеграм',
-        resume: 'Резюме',
-        resume_href: '#',
-    },
+  [LOCALES.ENGLISH]: {
+    name: 'Slava Lavrov',
+    profession: 'Front-end Developer',
+    menu_works: 'WORKS',
+    menu_about: 'ABOUT',
+    menu_lang_ru: 'RUS',
+    menu_lang_en: 'ENG',
+    menu_mail: 'MAIL',
+    project_minesweeper: 'Minesweeper',
+    project_minesweeper_description:
+      'It\'s a classical game written in JavaScript and React. The objective' +
+      ' of the game is to clear a rectangular board containing hidden mines without detonating any of them, with' +
+      ' help from clues about the number of neighboring mines in each field.',
+    project_piano: 'Virtual Piano',
+    project_piano_description:
+      'This is a virtual piano. Its feature is in the ability to automatically ' +
+      'play thousand of melodies using \'pseudo notes\' from the website that contains \'pseudo notes\' for ' +
+      'thousand of songs.',
+    project_word_counter: 'Word Counter',
+    project_word_counter_description:
+      'An app for word count that can help you learn English by getting the most' +
+      ' used words in a book before reading. It works with .epub files. Its main feature is called "Smart Count"' +
+      ' its ability to count words by their root (words with different forms count as the same one).',
+    technologies: 'technologies I use',
+    yandex_title: 'Yandex',
+    yandex_position: 'Tester Accessor',
+    yandex_description:
+      'Tested browser, mobile applications, web sites API’s using web debugging proxy tools ' +
+      'to mock and modify applications requests.',
+    yandex_href: 'https://yandex.com/company/general_info/yandex_today',
+    lexicom_title: 'Lexicom',
+    lexicom_position: 'Junior-Middle React Developer',
+    lexicom_description:
+      'Developed on the web text documents system that has templates, text blocks and autosave features.\n' +
+      '\n' +
+      'Made text editor 90% more efficient in text insertion, editing and styling by separating CPU-heavy tasks into macrotasks.\n' +
+      '\n' +
+      'Build state management system with middleware that connected application to the backend API, using Redux Saga.',
+    lexicom_href: 'https://lexicom.ai',
+    shri_title: 'Yandex Academy',
+    shri_position: 'Participant in Interface Development School',
+    shri_description: 'We had lectures, seminars, and workshops from Yandex employees who work every day on services' +
+      ' with a multi-million audience.\n After that, we worked on a team project under the guidance of a mentors.',
+    shri_href: 'https://academy.yandex.ru/schools/frontend',
+    yandex_internship_title: 'Internship at Yandex',
+    yandex_internship_position: 'Frontend Developer at Infrastructure team',
+    yandex_internship_description: 'Three-month internship at infrastructure team.',
+    yandex_internship_href: 'https://yandex.ru/yaintern/',
+    about_me_title: 'About me',
+    about_me_text:
+      'I\'m an inspired front-end developer. Sometimes I do web-design for my projects, like this website.' +
+      ' Currently, I code using TypeScript and React. I have' +
+      ' commercial experience in development and I also completed {school_name}. Right now I\'m an intern' +
+      ' at Yandex.',
+    yandex_school_name: 'Yandex developer school',
+    contacts: 'Contacts',
+    telegram: 'Telegram',
+    resume: 'Resume',
+    resume_href: '#',
+  },
+  [LOCALES.RUSSIAN]: {
+    name: 'Слава Лавров',
+    profession: 'Front-end Разработчик',
+    menu_works: 'РАБОТЫ',
+    menu_about: 'О СЕБЕ',
+    menu_lang_ru: 'РУС',
+    menu_lang_en: 'АНГ',
+    menu_mail: 'ПОЧТА',
+    project_minesweeper: 'Минёр',
+    project_minesweeper_description:
+      'Это классическая игра написанная на React и JavaScript. Цель игры, открыть все ' +
+      'клетки не содержащие мины. При открытии клетки отображается количество мин в соседних клетках.',
+    project_piano: 'Виртуально пианино',
+    project_piano_description:
+      'Это не просто пианино, его главная особенность в том, что оно может автоматически' +
+      ' проигрывать тысячи мелодий с помощью “псевдо нот”.',
+    project_word_counter: 'Word Counter',
+    project_word_counter_description:
+      'Приложение для подсчёта слов и изучения английского языка с помощью ' +
+      'чтения книг. Работает с файлами расширения .epub. У приложения есть режим "Smart Count" который считает' +
+      ' слова по их корню (слова с разными окончаниями считаются как одно слово).',
+    technologies: 'технологии которые я использую',
+    yandex_title: 'Яндекс',
+    yandex_position: 'Тестировщик Асессор',
+    yandex_description:
+      'Занимался тестированием браузеров, мобильных приложений, веб-сайтов и API. Использовал' +
+      ' инструменты отладки веб прокси для проверки и подменны запросов приложения.',
+    yandex_href: 'https://yandex.ru/company/main',
+    lexicom_title: 'Лексиком',
+    lexicom_position: 'Junior-Middle React Разработчик',
+    lexicom_description:
+      'Разработал систему текстовых документов, которая имеет шаблоны, текстовые блоки и авто сохранение.\n' +
+      '\n' +
+      'Оптимизировал текстовый редактор при вставке, редактировании и стилизации большого объёма текста с помощью разделения тяжёлых задач на макрозадачи.\n' +
+      '\n' +
+      'Разработал систему управления состояниям с middleware для отправки запросов на backend используя Redux Saga',
+    lexicom_href: 'https://lexicom.ai',
+    shri_title: 'Академия Яндекса',
+    shri_position: 'Участник Школы Разработки Интерфейсов',
+    shri_description: 'У нас были лекции, семинары и практикумы от сотрудников Яндекса, которые каждый день работают ' +
+      'над сервисами с многомиллионной аудиторией.\n' +
+      'Параллельно мы работали над командными проектами под руководством кураторов.',
+    shri_href: 'https://academy.yandex.ru/schools/frontend',
+    yandex_internship_title: 'Стажировка в Яндекс',
+    yandex_internship_position: 'Frontend разработчик в команде инфраструктуры',
+    yandex_internship_description: 'Трех месячная стажировка в команде инфраструктуры.',
+    yandex_internship_href: 'https://yandex.ru/yaintern/',
+    about_me_title: 'О себе',
+    about_me_text:
+      'Я frontend разработчик, иногда делаю дизайн для своих проектов, как например этот сайт. ' +
+      'Пишу код на TypeScript и React. У меня есть коммерческий опыт в разработки и pet проекты. ' +
+      'Я окончил {school_name} и теперь стажируюсь здесь.' +
+      '',
+    yandex_school_name: 'школу разработки интерфейсов Яндекса',
+    contacts: 'Контакты',
+    telegram: 'Телеграм',
+    resume: 'Резюме',
+    resume_href: '#',
+  },
 }
--- src/components/blocks/WorkPlaces/WorkPlaces.jsx
+++ src/components/blocks/WorkPlaces/WorkPlaces.jsx
@@ -2,69 +2,113 @@ import Place from './Place'
 import { FormattedDate, useIntl } from 'react-intl'
 
 export default function WorkPlaces() {
-    const Intl = useIntl()
+  const Intl = useIntl()
 
-    return (
-        <div className="work-places">
-            <div className="work-places__timing__left-padding" />
-            <h2 className="work-places__timing__year timing__item" style={{ gridRow: '2 / span 2' }}>
-                <FormattedDate value={new Date('2022')} year="numeric" />
-            </h2>
-            <div className="timing__months-container">
-                <p className="timing__item">
-                    <FormattedDate value={new Date('2022 june')} month="long" />
-                </p>
-                <p className="timing__item">
-                    <FormattedDate value={new Date('2022 may')} month="long" />
-                </p>
-                <p className="timing__item">
-                    <FormattedDate value={new Date('2022 april')} month="long" />
-                </p>
-                <p className="timing__item">
-                    <FormattedDate value={new Date('2022 march')} month="long" />
-                </p>
-                <p className="timing__item">
-                    <FormattedDate value={new Date('2022 february')} month="long" />
-                </p>
-                <p className="timing__item">
-                    <FormattedDate value={new Date('2022 january')} month="long" /> 😷
-                </p>
-            </div>
-            <h2 className="work-places__timing__year timing__item">
-                <FormattedDate value={new Date('2021')} year="numeric" />
-            </h2>
-            <h4 className="timing__item">
-                <FormattedDate value={new Date('2021 december')} month="long" /> 🎄
-            </h4>
-            <div style={{ gridRow: 'span 2' }} className="timing__months-container">
-                <p className="timing__item">
-                    <FormattedDate value={new Date('2021 november')} month="long" />
-                </p>
-                <p className="timing__item">
-                    <FormattedDate value={new Date('2021 october')} month="long" />
-                </p>
-                <p className="timing__item">
-                    <FormattedDate value={new Date('2021 september')} month="long" />
-                </p>
-            </div>
-            <h4 className="timing__item">
-                <FormattedDate value={new Date('2021 august')} month="long" />
-            </h4>
-            <div className="work-places__time-line" />
-            <Place
-                startRow={5}
-                companyName={Intl.formatMessage({ id: 'lexicom_title' })}
-                position={Intl.formatMessage({ id: 'lexicom_position' })}
-                description={Intl.formatMessage({ id: 'lexicom_description' })}
-                websiteHref={Intl.formatMessage({ id: 'lexicom_href' })}
-            />
-            <Place
-                companyName={Intl.formatMessage({ id: 'yandex_title' })}
-                position={Intl.formatMessage({ id: 'yandex_position' })}
-                description={Intl.formatMessage({ id: 'yandex_description' })}
-                websiteHref={Intl.formatMessage({ id: 'yandex_href' })}
-            />
-            <div className="work-places__timing__right-padding" />
-        </div>
-    )
+  return (
+    <div className="work-places">
+      <div className="work-places__timing__left-padding" />
+      <h2
+        className="work-places__timing__year timing__item"
+        style={{ gridRow: '2 / span 2' }}
+      >
+        <FormattedDate value={new Date('2023')} year="numeric" />
+      </h2>
+      <h4 className="timing__item">
+        <FormattedDate value={new Date('2023 january')} month="long" />
+      </h4>
+      <h2
+        className="work-places__timing__year timing__item"
+      >
+        <FormattedDate value={new Date('2022')} year="numeric" />
+      </h2>
+      <div className="timing__months-container">
+        <p className="timing__item">
+          <FormattedDate value={new Date('2022 december')} month="long" />
+        </p>
+        <p className="timing__item">
+          <FormattedDate value={new Date('2022 november')} month="long" />
+        </p>
+        <p className="timing__item">
+          <FormattedDate value={new Date('2022 october')} month="long" />
+        </p>
+      </div>
+      <h4 className="timing__item">
+        <FormattedDate value={new Date('2022 september')} month="long" />
+      </h4>
+      <div className="timing__months-container">
+        <p className="timing__item">
+          <FormattedDate value={new Date('2022 august')} month="long" />
+        </p>
+        <p className="timing__item">
+          <FormattedDate value={new Date('2022 july')} month="long" />
+        </p>
+        <p className="timing__item">
+          <FormattedDate value={new Date('2022 june')} month="long" />
+        </p>
+        <p className="timing__item">
+          <FormattedDate value={new Date('2022 may')} month="long" />
+        </p>
+        <p className="timing__item">
+          <FormattedDate value={new Date('2022 april')} month="long" />
+        </p>
+        <p className="timing__item">
+          <FormattedDate value={new Date('2022 march')} month="long" />
+        </p>
+        <p className="timing__item">
+          <FormattedDate value={new Date('2022 february')} month="long" />
+        </p>
+        <p className="timing__item">
+          <FormattedDate value={new Date('2022 january')} month="long" /> 😷
+        </p>
+      </div>
+      <h2 className="work-places__timing__year timing__item">
+        <FormattedDate value={new Date('2021')} year="numeric" />
+      </h2>
+      <h4 className="timing__item">
+        <FormattedDate value={new Date('2021 december')} month="long" /> 🎄
+      </h4>
+      <div style={{ gridRow: 'span 2' }} className="timing__months-container">
+        <p className="timing__item">
+          <FormattedDate value={new Date('2021 november')} month="long" />
+        </p>
+        <p className="timing__item">
+          <FormattedDate value={new Date('2021 october')} month="long" />
+        </p>
+        <p className="timing__item">
+          <FormattedDate value={new Date('2021 september')} month="long" />
+        </p>
+      </div>
+      <h4 className="timing__item">
+        <FormattedDate value={new Date('2021 august')} month="long" />
+      </h4>
+      <div className="work-places__time-line" />
+      <Place
+        startRow={3}
+        companyName={Intl.formatMessage({ id: 'yandex_internship_title' })}
+        position={Intl.formatMessage({ id: 'yandex_internship_position' })}
+        description={Intl.formatMessage({ id: 'yandex_internship_description' })}
+        websiteHref={Intl.formatMessage({ id: 'yandex_internship_href' })}
+      />
+      <Place
+        companyName={Intl.formatMessage({ id: 'shri_title' })}
+        position={Intl.formatMessage({ id: 'shri_position' })}
+        description={Intl.formatMessage({ id: 'shri_description' })}
+        websiteHref={Intl.formatMessage({ id: 'shri_href' })}
+      />
+      <Place
+        // startRow={5}
+        companyName={Intl.formatMessage({ id: 'lexicom_title' })}
+        position={Intl.formatMessage({ id: 'lexicom_position' })}
+        description={Intl.formatMessage({ id: 'lexicom_description' })}
+        websiteHref={Intl.formatMessage({ id: 'lexicom_href' })}
+      />
+      <Place
+        companyName={Intl.formatMessage({ id: 'yandex_title' })}
+        position={Intl.formatMessage({ id: 'yandex_position' })}
+        description={Intl.formatMessage({ id: 'yandex_description' })}
+        websiteHref={Intl.formatMessage({ id: 'yandex_href' })}
+      />
+      <div className="work-places__timing__right-padding" />
+    </div>
+  )
 }
